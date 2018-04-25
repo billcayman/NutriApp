5b3b228ea02abeb06b7f0990d0352710a08ec214
Bulat Gizyatullin <Bulat.Gizyatullin@icl-services.com>
2018-04-25 13:56:42 +0300

    add new files

---
+++ project/Database.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SQLite;
+using System.IO;
+
+namespace NutriApp
+{
+    class Database
+    {
+        public SQLiteConnection myConnection;
+
+        public Database()
+        {
+            myConnection = new SQLiteConnection("Data Source = database.sqlite3");
+            if (!File.Exists("./database.sqlite3"))
+            {
+                SQLiteConnection.CreateFile("database.sqlite3");
+                System.Console.WriteLine("Database file has been created.");
+            }
+            else 
+            {
+                System.Console.WriteLine("Database has been already created!");
+            }
+            
+        }
+
+    }
+}
---
+++ project/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.IO;
+
+namespace NutriApp
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Database databaseObject = new Database();
+
+            Console.ReadKey();
+        }
+    }
+}
